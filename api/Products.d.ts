78a9b5da0f6f0e0de1f5a8605da790f1fca838ee
Mehul Makwana <mehulmakwana97@gmail.com>
2019-03-15 16:18:09 +0530

    Removed unused files

--- api/Products.d.ts
+++
@@ -1,7 +0,0 @@
-import { Resource } from "../common/Resource";
-import { Shopify } from "../Shopify";
-export declare class Products extends Resource {
-    constructor(config: Shopify, id?: any[]);
-    collectionId(cid: any): this;
-    presentmentCurrencies(currency: any): this;
-}
